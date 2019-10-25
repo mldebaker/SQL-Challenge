6c846fc2d89b2b7e2c916763397ba4cd9790575f
mldebaker <mldebaker@gmail.com>
2019-10-25 01:29:44 -0600

    create first list/question answer

--- EmployeeSQL/QuickDBD-SQL-Challenge.sql
+++ EmployeeSQL/QuickDBD-SQL-Challenge.sql
@@ -77,3 +77,10 @@ SELECT * FROM dept_manager;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+--List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
+FROM employees e
+LEFT JOIN salaries s
+ON e.emp_no = s.emp_no;
+
