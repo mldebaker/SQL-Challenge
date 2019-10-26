afd8ea01c0499da7c4f424371c5f7d8577b44a81
mldebaker <mldebaker@gmail.com>
2019-10-26 01:42:18 -0600

    final update

--- EmployeeSQL/QuickDBD-SQL-Challenge.sql
+++ EmployeeSQL/QuickDBD-SQL-Challenge.sql
@@ -78,9 +78,55 @@ SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
 
---List the following details of each employee: employee number, last name, first name, gender, and salary.
+--1. List the following details of each employee: employee number, last name, 
+--first name, gender, and salary.
 SELECT e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 FROM employees e
 LEFT JOIN salaries s
 ON e.emp_no = s.emp_no;
 
+--2. List employees who were hired in 1986.
+SELECT * FROM employees WHERE
+hire_date>= '1986-01-01' and
+hire_date< '1987-01-01';
+
+--3. List the manager of each department with the following information: department number, 
+--department name, the manager's employee number, last name, first name, and start and end 
+--employment dates.
+SELECT dm.dept_no AS department_no, d.dept_name, dm.emp_no, e.first_name, e.last_name, dm.from_date, dm.to_date
+FROM employees e
+INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no
+INNER JOIN departments d ON dm.dept_no = d.dept_no;
+
+--4. List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
+FROM employees e
+INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no
+INNER JOIN departments d ON dm.dept_no = d.dept_no;
+
+--5. List all employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name 
+FROM employees 
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--6. List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
+SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
+FROM employees e
+INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no
+INNER JOIN departments d ON dm.dept_no = d.dept_no
+WHERE dept_name IN('Sales');
+
+--7. List all employees in the Sales and Development departments, including their 
+--employee number, last name, first name, and department name.
+SELECT d.dept_name, e.emp_no, e.first_name, e.last_name
+FROM employees e
+INNER JOIN dept_manager dm ON e.emp_no = dm.emp_no
+INNER JOIN departments d ON dm.dept_no = d.dept_no
+WHERE dept_name IN('Sales', 'Development');
+
+--8. In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) FROM employees 
+GROUP BY last_name ORDER BY COUNT desc;
